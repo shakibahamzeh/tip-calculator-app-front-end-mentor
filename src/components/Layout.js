12c0174d4531176aea005065cf5373cf06925df3
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2022-07-12 00:34:15 +0430

    responsive and add functions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "../assets/styles/layout.css";
+import dollar from "../assets/images/icon-dollar.svg";
+import person from "../assets/images/icon-person.svg";
 
 const Layout = () => {
+  const [bill,setBill] = useState(0);
+  const [tip , setTip] = useState(0.05);
+  const [people,setPerson] = useState(0);
+  const [tipAmount,setTipAmount] = useState(0);
+  const [total,setTotal] = useState(0);
+
+
+  const clickHandler = (e) => {
+    setTip(parseInt(e.target.textContent)/100);
+  }
+  const changeHandler = (e) => {
+    if( e.target.value === ""){
+     setBill(0)
+    } else if(e.target.value !== Number){
+       setBill(parseInt(e.target.value))
+   }}
+   const personHandler = (e) => {
+    if( e.target.value === ""){
+     setPerson(0)
+    } else if(e.target.value !== Number){
+       setPerson(parseInt(e.target.value))
+   }}
+ 
+   if(people !== 0 & bill !==0){
+     setTipAmount(((tip*bill)/people).toFixed(2));
+     setTotal((bill / people) + tipAmount);
+   }else{
+     setTipAmount(0)
+     setTotal(0)
+   }
+ 
   return (
     <div className='layout'>
       <section className='right-container'>
           <div className='bill-container'>
               <label htmlFor='bill'>Bill</label>
-              <input type="text" id='bill' placeholder='0'/>
-              <span>$</span>
+              <input type="text" id='bill' placeholder='0' value={bill} onChange={changeHandler}/>
+              <div>
+                <img src={dollar} alt="dollar"/>
+              </div>
           </div>
           <div className='tip-container'>
               <h4>Select Tip %</h4>
-             <section className='tip-btns'>
+             <section className='tip-btns' onClick={clickHandler} value={people}>
                   <div>
                    5%
                   </div>
@@ -35,7 +70,10 @@ const Layout = () => {
           </div>
           <div className='people-container'>
               <label htmlFor='people'>Number Of People</label>
-              <input type="text" id='people' placeholder='0'/>
+              <input type="text" id='people' placeholder='0' onChange={personHandler}/>
+              <div>
+                <img src={person} alt="person"/>
+              </div>
           </div>
       </section>
       <section className='total-container'>
@@ -45,7 +83,7 @@ const Layout = () => {
                   <span>/person</span>
               </div>
               <div>
-                  <h1>$4.27</h1>
+                  <h1>$ {tipAmount}</h1>
               </div>
           </div>
           <div className='child-total'>
@@ -54,7 +92,7 @@ const Layout = () => {
                   <span>/person</span>
               </div>
               <div>
-                  <h1>$32.79</h1>
+                  <h1>$ {total}</h1>
               </div>
           </div>
           <div className='reset-btn'>
