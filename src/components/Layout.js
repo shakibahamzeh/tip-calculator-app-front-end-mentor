f359e31bb6af65f8eff5c899b6d4ff7af187d7b9
shakiba Hamzeh <shakiba.h_70@yahoo.com>
2022-07-15 17:32:08 +0430

    set bill and tip and reset button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,35 +5,46 @@ import person from "../assets/images/icon-person.svg";
 
 const Layout = () => {
   const [bill,setBill] = useState(0);
-  const [tip , setTip] = useState(0.05);
+  const [tip , setTip] = useState(0);
   const [people,setPerson] = useState(0);
-  const [tipAmount,setTipAmount] = useState(0);
-  const [total,setTotal] = useState(0);
+  const [touched , setTouched] = useState(false);
+  const [custom,setCustom] = useState('')
 
 
   const clickHandler = (e) => {
     setTip(parseInt(e.target.textContent)/100);
+   
   }
+
   const changeHandler = (e) => {
     if( e.target.value === ""){
      setBill(0)
-    } else if(e.target.value !== Number){
-       setBill(parseInt(e.target.value))
+    } else if(!isNaN(e.target.value)){
+       setBill(parseFloat(e.target.value))
    }}
    const personHandler = (e) => {
     if( e.target.value === ""){
      setPerson(0)
     } else if(e.target.value !== Number){
        setPerson(parseInt(e.target.value))
-   }}
- 
-   if(people !== 0 & bill !==0){
-     setTipAmount(((tip*bill)/people).toFixed(2));
-     setTotal((bill / people) + tipAmount);
-   }else{
-     setTipAmount(0)
-     setTotal(0)
+   }};
+
+   const customHandler = (e) => {
+     if(e.target.value === ""){
+       setCustom('');
+     }else if(e.target.value !== Number){
+      const tip = setCustom(parseInt(e.target.value));
+       console.log(tip)
+     }
    }
+
+ 
+  const resetHandler = () => {
+    setBill(0);
+    setTip(0);
+    setPerson(0);
+    setTouched(false)
+  };
  
   return (
     <div className='layout'>
@@ -47,7 +58,7 @@ const Layout = () => {
           </div>
           <div className='tip-container'>
               <h4>Select Tip %</h4>
-             <section className='tip-btns' onClick={clickHandler} value={people}>
+             <section className={`tip-btns ${touched ? "touched-btn" : ""}`} onClick={clickHandler} value={people}>
                   <div>
                    5%
                   </div>
@@ -64,14 +75,17 @@ const Layout = () => {
                     50%
                   </div>
                   <div>
-                    <input type="text" placeholder='Custum'/>
+                    <input type="text" placeholder='Custum'  value={custom} onChange={customHandler}/>
                   </div>
              </section>
           </div>
-          <div className='people-container'>
-              <label htmlFor='people'>Number Of People</label>
-              <input type="text" id='people' placeholder='0' onChange={personHandler}/>
-              <div>
+          <div  className={`people-container ${touched && people === 0 ? "touched-input" : ""}`}>
+              <div className='warning-container'>
+                  <label htmlFor='people'>Number Of People</label>
+                  <span>Can't be zero</span>
+              </div>
+              <input type="text" id='people' placeholder='0' onChange={personHandler} className={`touched && ${people === 0} ? "touched-input" : ""`} onClick={e => setTouched(true)} value={people}/>
+              <div className='img-wrapper'>
                 <img src={person} alt="person"/>
               </div>
           </div>
@@ -83,7 +97,11 @@ const Layout = () => {
                   <span>/person</span>
               </div>
               <div>
-                  <h1>$ {tipAmount}</h1>
+                  <h1>$ 
+                    {
+                      (bill !== 0 && people !== 0) ? ((bill * tip)/people).toFixed(2) : 0
+                    }
+                  </h1>
               </div>
           </div>
           <div className='child-total'>
@@ -92,11 +110,15 @@ const Layout = () => {
                   <span>/person</span>
               </div>
               <div>
-                  <h1>$ {total}</h1>
+                  <h1>$ 
+                    {
+                      (bill !== 0 && people !== 0) ? ((bill * tip)/people + (bill/people)) : 0
+                    }
+                  </h1>
               </div>
           </div>
-          <div className='reset-btn'>
-              <button>Reset</button>
+          <div className={`reset-btn ${bill === 0 && people === 0 ? '' : 'active'}`}>
+              <button onClick={resetHandler}>Reset</button>
           </div>
       </section>
     </div>
